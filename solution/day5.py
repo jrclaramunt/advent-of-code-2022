7648a86124a3b0271a4bd320f768564fe6a601e6
J. Ramon Claramunt <jr.claramunt@gmail.com>
2022-12-06 22:32:02 +0100

    Refactor day 5 solution

--- solution/day5.py
+++ solution/day5.py
@@ -24,36 +24,31 @@ class Crate:
 class Day5(Day):
 
     def __init__(self, args):
-        self.input = list(map(lambda x: x, args[0]))
+        data_input = list(map(lambda x: x, args[0]))
+        movements_line = self.get_movements_index(data_input)
 
-        for i in self.input:
-            if re.match(r'( ?\d  ?)', i):
-                break
-
-        movements_line = self.init_cranes()
-        self.supply_movement = self.input[movements_line:]
+        self.crates_initial_state = data_input[:movements_line - 2]
+        self.supply_movement = data_input[movements_line:]
+        self.action_regex = r'move (\d+) from (\d+) to (\d+)'
 
     def part1(self):
-        action_regex = r'move (\d+) from (\d+) to (\d+)'
+        crates = self.init_cranes()
+
         for i in range(len(self.supply_movement)):
-            data = re.match(action_regex, self.supply_movement[i].strip()).groups()
+            data = re.match(self.action_regex, self.supply_movement[i].strip()).groups()
 
             units_to_move = int(data[0])
             origin = int(data[1])
             destination = int(data[2])
 
-            for i in range(units_to_move):
-                supply = self.crates[origin].pop()
-                self.crates[destination].push(supply)
+            for _ in range(units_to_move):
+                supply = crates[origin].pop()
+                crates[destination].push(supply)
 
-        result = ''
-        for i in range(len(self.crates)):
-            result += self.crates[i+1].pop()
-
-        return result
+        return self.get_cranes_top(crates)
 
     def part2(self):
-        self.init_cranes()
+        crates = self.init_cranes()
         action_regex = r'move (\d+) from (\d+) to (\d+)'
         for i in range(len(self.supply_movement)):
             data = re.match(action_regex, self.supply_movement[i].strip()).groups()
@@ -63,32 +58,34 @@ class Day5(Day):
             destination = int(data[2])
 
             batch = []
-            for j in range(units_to_move):
-                supply = self.crates[origin].pop()
+            for _ in range(units_to_move):
+                supply = crates[origin].pop()
                 batch.append(supply)
 
             batch.reverse()
             for j in batch:
-                self.crates[destination].push(j)
+                crates[destination].push(j)
 
-        result = ''
-        for i in range(len(self.crates)):
-            result += self.crates[i + 1].pop()
-
-        return result
+        return self.get_cranes_top(crates)
 
     def init_cranes(self):
         regex_supplies = r'(   |\[[A-Z]\] ?|\\n)'
-        self.crates = defaultdict(lambda: Crate())
-        movements_line = 0
-        for i in self.input:
-            if re.match(r'( ?\d  ?)', i):
-                break
+        crates = defaultdict(lambda: Crate())
 
+        for i in self.crates_initial_state:
             data = re.findall(regex_supplies, i)
             for i in range(len(data)):
                 if data[i].strip() != '':
-                    self.crates[i + 1].add(data[i].strip())
+                    crates[i + 1].add(data[i].strip())
 
-            movements_line += 1
-        return movements_line + 2
+        return crates
+
+    @staticmethod
+    def get_cranes_top(crates):
+        return ''.join(map(lambda x: crates[x + 1].pop(), range(len(crates))))
+
+    @staticmethod
+    def get_movements_index(input_data):
+        for i in range(len(input_data)):
+            if re.match(r'( ?\d  ?)', input_data[i]):
+                return i + 2
